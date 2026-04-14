68be12d765ff5b4081f578e21cf19dfa8da97e43
Dinh Nguyen <127725498+ntqdinh-axonivy@users.noreply.github.com>
2026-04-14 14:28:54 +0700

    MARP-3774 SFTP Connector has path traversal  (#68 )

--- sftp-connector-test/src_test/com/axonivy/connector/sftp/test/BaseTest.java
+++ sftp-connector-test/src_test/com/axonivy/connector/sftp/test/BaseTest.java
@@ -1,10 +1,11 @@
 package com.axonivy.connector.sftp.test;
 
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
-import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.environment.IvyTest;
+import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
+import ch.ivyteam.ivy.environment.AppFixture;
+import static com.axonivy.connector.sftp.constants.IvyVariableNames.*;
 
-@IvyTest
+@IvyProcessTest
 public class BaseTest {
 	protected static final String TEST_SFTP_SERVER_NAME = "dummy";
 	protected static final String TEST_SFTP_SSH_SERVER_NAME = "dummy_ssh";
@@ -13,22 +14,30 @@ public class BaseTest {
 	protected static final BpmProcess TEST_UPLOAD_FILE_PROCESS = BpmProcess.path("Sftp/SftpUploadFile");
 	protected static final BpmProcess TEST_DOWNLOAD_FILE_PROCESS = BpmProcess.path("Sftp/SftpDownloadFile");
 
-	protected static final String PREFIX = "com.axonivy.connector.sftp.server";
 	protected static final String TEST_FILE_NAME = "market_market_connector_sftp.pdf";
 	protected static final long TEST_FILE_SIZE = 207569L;
 	
-	protected static void setVarForSFTPName(String sftpServerName, String username, String auth, String password, String sshKeyFilePath, String sshpassphrase) {
-		setVar(sftpServerName, "host", "localhost");
-		setVar(sftpServerName, "username", username);
-		setVar(sftpServerName, "port", "22");
-		setVar(sftpServerName, "auth", auth);
-		setVar(sftpServerName, "password", password);
-		setVar(sftpServerName, "sshkeyFilePath", sshKeyFilePath);
-		setVar(sftpServerName, "sshPassphraseSecret", sshpassphrase);
+	protected static void setVarForSFTPName(String sftpServerName, String username, String auth, String password,
+			String sshKeyFilePath, String sshpassphrase, AppFixture fixture) {
+		setVarForSFTPName(sftpServerName, username, auth, password, sshKeyFilePath, sshpassphrase, "false", "", fixture);
 	}
-	
-	private static void setVar(String sftpServerName, String var, String value) {
-		Ivy.var().set(String.format("%s.%s.%s", PREFIX, sftpServerName, var), value);
+
+	protected static void setVarForSFTPName(String sftpServerName, String username, String auth, String password,
+			String sshKeyFilePath, String sshpassphrase, String enforcePathRestrictions, String baseLocalDir, AppFixture fixture) {
+		setVar(sftpServerName, HOST_VAR, "localhost", fixture);
+		setVar(sftpServerName, USERNAME_VAR, username, fixture);
+		setVar(sftpServerName, PORT_VAR, "22", fixture);
+		setVar(sftpServerName, AUTH_VAR, auth, fixture);
+		setVar(sftpServerName, PASSWORD_VAR, password, fixture);
+		setVar(sftpServerName, SSHKEY_FILEPATH_VAR, sshKeyFilePath, fixture);
+		setVar(sftpServerName, SECRET_SSHPASSPHRASE_VAR, sshpassphrase, fixture);
+		setVar(sftpServerName, ENFORCE_PATH_RESTRICTIONS_VAR, enforcePathRestrictions, fixture);
+		setVar(sftpServerName, BASE_LOCAL_DIR_VAR, baseLocalDir, fixture);
+		setVar(sftpServerName, STRICT_HOST_KEY_CHECKING_VAR, "no", fixture);
+	}
+
+	protected static void setVar(String sftpServerName, String var, String value, AppFixture fixture) {
+		fixture.var(String.format("%s.%s.%s", SFTP_VAR, sftpServerName, var), value);
 	}
 	
 }
---
+++ sftp-connector/src/com/axonivy/connector/sftp/constants/IvyVariableNames.java
@@ -0,0 +1,18 @@
+package com.axonivy.connector.sftp.constants;
+
+public class IvyVariableNames {
+	public static final String HOST_VAR = "host";
+	public static final String PORT_VAR = "port";
+	public static final String SECRET_SSHPASSPHRASE_VAR = "sshPassphraseSecret";
+	public static final String SSHKEY_FILEPATH_VAR = "sshkeyFilePath";
+	public static final String AUTH_VAR = "auth";
+	public static final String PASSWORD_VAR = "password";
+	public static final String USERNAME_VAR = "username";
+	public static final String BASE_LOCAL_DIR_VAR = "baseLocalDir";
+	public static final String ENFORCE_PATH_RESTRICTIONS_VAR = "enforcePathRestrictions";
+	public static final String STRICT_HOST_KEY_CHECKING_VAR = "strictHostKeyChecking";
+	public static final String SFTP_VAR = "com.axonivy.connector.sftp.server";
+
+	private IvyVariableNames() {
+	}
+}
