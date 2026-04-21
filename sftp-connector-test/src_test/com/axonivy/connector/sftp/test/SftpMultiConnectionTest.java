a8ed96496bdfa6170a2f1043f30631030dfe1c68
Dinh Nguyen <127725498+ntqdinh-axonivy@users.noreply.github.com>
2026-04-21 08:39:58 +0700

    MARP-3774 SFTP Connector has path traversal  (#68 ) (#73)

--- sftp-connector-test/src_test/com/axonivy/connector/sftp/test/SftpMultiConnectionTest.java
+++ sftp-connector-test/src_test/com/axonivy/connector/sftp/test/SftpMultiConnectionTest.java
@@ -10,29 +10,25 @@ import org.junit.jupiter.api.Test;
 import com.axonivy.connector.sftp.service.SftpClientService;
 
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
-import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
+import ch.ivyteam.ivy.environment.AppFixture;
 
 
 /**
  * This SftpMultiConnectionTest creates 2 sFTP connections
  */
-@IvyProcessTest(enableWebServer = true)
 public class SftpMultiConnectionTest extends BaseTest {
 
-	private static final String SFTP_NAME = "dummy";
-	private static final String SFTP_SSH_NAME = "dummy_ssh";
-
 	@BeforeEach
-	public void preInit() throws Exception {
-		setVarForSFTPName(TEST_SFTP_SERVER_NAME, "usr", "password", "pwd", "", "");
+	public void preInit(AppFixture fixture) throws Exception {
+		setVarForSFTPName(TEST_SFTP_SERVER_NAME, "usr", "password", "pwd", "", "", fixture);
 		String keyPath = SftpProcessSSHTest.class.getResource("sftptest").getPath();
-		setVarForSFTPName(TEST_SFTP_SSH_SERVER_NAME, "usr2ssh", "ssh", "", keyPath, "123456");
+		setVarForSFTPName(TEST_SFTP_SSH_SERVER_NAME, "usr2ssh", "ssh", "", keyPath, "123456", fixture);
 	}
 
 	@Test
 	public void callOpenConnection(BpmClient bpmClient) throws IOException {
-		SftpClientService sftpClient = new SftpClientService(SFTP_NAME);
-		SftpClientService sftpSSHClient = new SftpClientService(SFTP_SSH_NAME);
+		SftpClientService sftpClient = new SftpClientService(TEST_SFTP_SERVER_NAME);
+		SftpClientService sftpSSHClient = new SftpClientService(TEST_SFTP_SSH_SERVER_NAME);
 
 		assertThat(sftpClient).isNotNull();
 		assertThat(sftpSSHClient).isNotNull();
